fd5bf7267d420c5e0e019dd11a49d28939a4c090
King0206 <86298193+King0206@users.noreply.github.com>
2021-10-10 22:46:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const CONFIG = {
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, anh à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnReply: 'Gửi cho anh <33',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Hôm nào đi ăn với anh nhéee công chúa.',
